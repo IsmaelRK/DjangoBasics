36f16d242383b0d319a3b5c54d3c3462dd7a518c
Ismael <ismael2021ab@gmail.com>
2023-10-09 20:55:56 -0300

    feat: Many Things

--- myapp/firstapp/views.py
+++ myapp/firstapp/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+
+import math
 # Create your views here.
+
+
+def pagina2(request, values):
+
+    try:
+        delta = (pow(values[1], 2)) - (4 * values[0] * values[2])
+
+        if delta < 0:
+
+            return render(request, 'index2.html', {'menor': delta})
+
+        elif delta == 0:
+
+            x = (-values[1] + 0) / (2 * values[0])
+            return render(request, 'index2.html', {'value': x})
+
+        else:
+            x = (-values[1] - math.sqrt(delta)) / (2 * values[0])
+            x2 = (-values[1] + math.sqrt(delta)) / (2 * values[0])
+            val = x, x2
+
+            return render(request, 'index2.html', {'values': val})
+
+    except Exception as error:
+
+        return render(request, 'index2.html', {'error': error})
+
+def pagina(request):
+
+    if request.method == 'POST':
+
+        a = request.POST.get('a')
+        b = request.POST.get('b')
+        c = request.POST.get('c')
+
+        a = int(a)
+        b = int(b)
+        c = int(c)
+
+
+        vls = a, b, c
+
+        return pagina2(request, vls)
+
+    return render(request, 'index.html')
--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import views
+from firstapp import views  # Search /firstapp from the "first" myapp
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.pagina, name="pagina"),  # Create view in views.py
+    path('result/', views.pagina2, name="pagina2")
 ]
